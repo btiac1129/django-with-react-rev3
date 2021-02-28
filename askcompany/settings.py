8e8e694f64d2d28ed6f09a0816b00fa7b3df4539
btiac1129 <btiac1129@gmail.com>
2021-02-28 23:09:19 +0900

    JWT 토큰'

--- askcompany/settings.py
+++ askcompany/settings.py
@@ -126,7 +126,8 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ],
     'PAGE_SIZE': 100,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
@@ -140,5 +141,9 @@ REST_FRAMEWORK = {
         # 'anon': None,
         'user': '3/day',
     }
+}
+
+JWT_AUTH = {
+    'JWT_ALLOW_REFRESH': True,
 }
 
--- test_token.py
+++ test_token.py
@@ -1,9 +1,15 @@
 import requests # pip install requests
 
-TOKEN = '2510279efb225800e99a6c34a72820123ca20c58'
+# TOKEN = '2510279efb225800e99a6c34a72820123ca20c58'
+JWT_TOKEN = (
+    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozLCJ1c2Vy"
+    "bmFtZSI6InVzZXIyIiwiZXhwIjoxNjE0NTE5NjYwLCJlbWFpbCI6IiIsIm9ya"
+    "WdfaWF0IjoxNjE0NTE5MzYwfQ.PWA5l_ITu4YBY1VUlejXDh5HAmH2cs6XyorpwOOcwzU"
+)
 
 headers = {
-    'Authorization': f'Token {TOKEN}'
+    # 'Authorization': f'Token {TOKEN}'
+    'Authorization': f'JWT {JWT_TOKEN}', # JWT 인증
 }
 
 res = requests.get('http://localhost:8000/post/1/', headers=headers)
